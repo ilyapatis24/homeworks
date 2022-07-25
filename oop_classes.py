cc624c4b42c8ce14fb94530d7259a00764faf54f
ilyapatis24 <ilyapatis24@mail.ru>
2022-07-25 21:06:36 +0300

    Доработка по заданию преподавателя

--- oop_classes.py
+++ oop_classes.py
@@ -1,3 +1,4 @@
+import math
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -19,39 +20,52 @@ class Student:
         marks = [0,1,2,3,4,5,6,7,8,9,10]
         if isinstance(lecturer, Lecturer) and grade in marks:
             if course in lecturer.grades:
-                lecturer.grades[course] += grade
+                lecturer.grades[course] += [grade]
             else:
-                lecturer.grades[course] = grade
+                lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
-    def aver_student(self):
-        return round(sum(self.grades.values())/len(self.grades),1)
-    def __str__(self):
-        return print(f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.aver_student()}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}")
+    def __str__(self,student):
+        return print(f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {aver_stud(student)}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}")
     def __lt__(self, other):
-        if self.aver_student() >= other.aver_student():
+        if aver_stud(self) >= aver_stud(other):
             return
-        return self.aver_student() < other.aver_student()
+        return aver_stud(self) < aver_stud(other)
+def aver_lect(lecturer):
+    count = 0
+    countlen = 0
+    for value in lecturer.grades.values():
+        countlen +=len(value)
+        for item in value:
+            count += item
+    return round(count/countlen, 2)
+    
+def aver_stud(student):
+    count = 0
+    countlen = 0
+    for value in student.grades.values():
+        countlen +=len(value)
+        for item in value:
+            count += item
+    return round(count/countlen, 2)
     
 class Lecturer(Mentor):
     def __init__(self,name,surname):
         super().__init__(name,surname)
         self.grades = {}     
-    def aver_lect(self):
-        return round(sum(self.grades.values())/len(self.grades),1)
-    def __str__(self):
-        return print(f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.aver_lect()}")
+    def __str__(self,lecturer):
+        return print(f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {aver_lect(lecturer)}")
     def __lt__(self, other):
-        if self.aver_lect() >= other.aver_lect():
+        if aver_lect(self) >= aver_lect(other):
             return
-        return self.aver_lect() < other.aver_lect()
+        return aver_lect(self) < aver_lect(other)
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student):
             if course in student.grades:
-                student.grades[course] += grade
+                student.grades[course] += [grade]
             else:
-                student.grades[course] = grade
+                student.grades[course] = [grade]
         else:
             return 'Ошибка'
             
@@ -83,12 +97,10 @@ reviewer2.rate_hw(student2, "Php", 4)
 reviewer2.rate_hw(student2, "Golang", 5)
 print(student1.grades)
 print(lecturer1.grades)
-print(student1.aver_student())
-print(lecturer1.aver_lect())
-print(student2.aver_student())
-print(lecturer2.aver_lect())
-student1.__str__()
-lecturer1.__str__()
+print(aver_lect(lecturer1))
+print(aver_stud(student1))
+student1.__str__(student1)
+lecturer1.__str__(lecturer1)
 reviewer1.__str__()
 print(lecturer2.__lt__(lecturer1))
 print(student2.__lt__(student1))
