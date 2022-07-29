2da27c8fb29d512d9adc07c3c0309c8014d9135b
ilyapatis24 <ilyapatis24@mail.ru>
2022-07-30 00:37:04 +0300

    mission2 cook_book part2

--- hw_readfile/readfile.py
+++ hw_readfile/readfile.py
@@ -1,3 +1,9 @@
+from asyncio import new_event_loop
+from hashlib import new
+from tokenize import cookie_re
+from types import new_class
+
+
 def my_cook_book():
     with open('cook.txt', encoding='utf-8') as file:
         cook_book = {}
@@ -8,5 +14,20 @@ def my_cook_book():
                 ingredient_name, quantity, measure = map(lambda x: int(x) if x.isdigit() else x, arg.split(' | '))
                 cook_li.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
             cook_book[name] = cook_li
-    return print(cook_book)
-my_cook_book()
+    return cook_book
+
+def get_shop_list_by_dishes(dishes,person_count):
+    new_cook = {}
+    cook_book = my_cook_book()
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            new_cook[dish] = ingredient
+            new_cook[dish]['quantity'] *= person_count
+    dic_dish = {}
+    for value in new_cook.values():
+        name = value['ingredient_name']
+        del value['ingredient_name']
+        dic_dish[name] = value
+    print(dic_dish)
+        
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'],2)
