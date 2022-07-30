4e2e39a7b6c93d949b826a8c4bf64e5450724cbc
ilyapatis24 <ilyapatis24@mail.ru>
2022-07-31 02:24:31 +0300

    mission 3 is completed

--- hw_readfile/readfile.py
+++ hw_readfile/readfile.py
@@ -27,3 +27,31 @@ def get_shop_list_by_dishes(dishes,person_count):
     print(dic_dish)
         
 get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'],2)
+
+import os
+import os.path
+
+def strings_count(file):
+    with open(file, 'r', encoding= 'utf-8') as f:
+        return sum(1 for line in f)
+    
+base_path = os.getcwd()
+location = os.path.abspath('D:/Мои документы/Илья/Обучение/Нетология/Python/домашние работы/homeworks/hw_readfile/files')
+file_for_write = os.path.abspath('D:/Мои документы/Илья/Обучение/Нетология/Python/домашние работы/homeworks/hw_readfile/123.txt')
+full_path = os.path.join(base_path, location)
+def rewrite(full_path, file_for_write):
+    files = []
+    for i in list(os.listdir(full_path)):
+        files.append([strings_count(os.path.join(full_path, i)), os.path.join(base_path, location, i), i])
+    for file_item in sorted(files):
+        opening_files = open(file_for_write, 'a', encoding= 'utf-8')
+        opening_files.write(f'{file_item[2]}\n')
+        opening_files.write(f'{file_item[0]}\n')
+        with open(file_item[1], 'r', encoding= 'utf-8') as file:
+            counting = 1
+            for line in file:
+                opening_files.write(f'строка № {counting} в файле {file_item[2]} : {line}')
+                counting += 1
+        opening_files.write(f'\n')
+        opening_files.close()
+rewrite(full_path, file_for_write)
